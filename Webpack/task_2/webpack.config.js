0df93699af07f996cdf95762efcae6d0d57200e3
ksyv <ksyviais@gmail.com>
2025-07-23 10:48:42 +0200

    add file-loader in modules in webpack config

--- Webpack/task_2/webpack.config.js
+++ Webpack/task_2/webpack.config.js
@@ -25,6 +25,10 @@ module.exports = {
           },
         ],
       },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        use: ['file-loader'],
+      },
     ],
   },
   performance: {
