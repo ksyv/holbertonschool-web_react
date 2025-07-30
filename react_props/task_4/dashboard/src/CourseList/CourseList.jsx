2022118c1a9212ccc62a44fb11d73af0a1cebc59
ksyv <ksyviais@gmail.com>
2025-07-30 11:25:30 +0200

    fix courselist

--- react_props/task_4/dashboard/src/CourseList/CourseList.jsx
+++ react_props/task_4/dashboard/src/CourseList/CourseList.jsx
@@ -1,48 +1,43 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import CourseListRow from './CourseListRow';
 import './CourseList.css';
 
 function CourseList({ courses = [] }) {
-  return (
-    <table id="CourseList">
-      <thead>
-        <CourseListRow textFirstCell="Available courses" isHeader={true} />
-        <CourseListRow textFirstCell="Course name" textSecondCell="Credit" isHeader={true} />
-      </thead>
-      <tbody>
-        {courses.length > 0 ? (
-          courses.map(course => (
-            <CourseListRow
-              key={course.id}
-              textFirstCell={course.name}
-              textSecondCell={course.credit}
-              isHeader={false}
-            />
-          ))
-        ) : (
-          <CourseListRow textFirstCell="No course available yet" isHeader={false} />
-        )}
-      </tbody>
-    </table>
-  );
+    if (courses.length === 0) {
+        return (
+            <div className='CourseListContainer'>
+                <table id="CourseList">
+                    <tbody>
+                        <CourseListRow textFirstCell="No course available yet" />
+                    </tbody>
+                </table>
+            </div>
+        );
+    }
+
+    return (
+        <div className='CourseListContainer'>
+            <table id="CourseList">
+                <thead>
+                    <CourseListRow textFirstCell="Available courses" isHeader={true} />
+                    <CourseListRow
+                        textFirstCell="Course name"
+                        textSecondCell="Credit"
+                        isHeader={true}
+                    />
+                </thead>
+                <tbody>
+                    {courses.map(course => (
+                        <CourseListRow
+                            key={course.id}
+                            textFirstCell={course.name}
+                            textSecondCell={course.credit.toString()}
+                        />
+                    ))}
+                </tbody>
+            </table>
+        </div>
+    );
 }
-
-CourseList.propTypes = {
-  courses: PropTypes.arrayOf(
-    PropTypes.shape({
-      id: PropTypes.number.isRequired,
-      type: PropTypes.string.isRequired,
-      value: PropTypes.string,
-      html: PropTypes.shape({
-        __html: PropTypes.string,
-      }),
-    })
-  ),
-};
-
-CourseList.defaultProps = {
-  courses: [],
-};
 
 export default CourseList;
