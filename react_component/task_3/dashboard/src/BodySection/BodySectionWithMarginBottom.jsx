6a0546b17905d05211a10e0edf8653d15a275118
ksyv <ksyviais@gmail.com>
2025-09-10 14:47:26 +0200

    change for adapt this margin

--- react_component/task_3/dashboard/src/BodySection/BodySectionWithMarginBottom.jsx
+++ react_component/task_3/dashboard/src/BodySection/BodySectionWithMarginBottom.jsx
@@ -1,36 +1,15 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+import BodySection from './BodySection';
+import './BodySection.css';
 
-
-function BodySection({ title, children }) {
-  return (
-    <div className="bodySection">
-      <h2>{title}</h2>
-      {children}
-    </div>
-  );
+function BodySectionWithMarginBottom({ title, children }) {
+    return (
+        <div className="bodySectionWithMargin">
+            <BodySection title={title}>
+                {children}
+            </BodySection>
+        </div>
+    );
 }
-
-BodySection.propTypes = {
-  title: PropTypes.string.isRequired,
-  children: PropTypes.node.isRequired,
-};
-
-
-function BodySectionWithMarginBottom(props) {
-  return (
-    <>
-      <style>{bodySectionWithMarginStyles}</style>
-      <div className="bodySectionWithMargin">
-        <BodySection {...props} />
-      </div>
-    </>
-  );
-}
-
-BodySectionWithMarginBottom.propTypes = {
-  title: PropTypes.string.isRequired,
-  children: PropTypes.node.isRequired,
-};
 
 export default BodySectionWithMarginBottom;
