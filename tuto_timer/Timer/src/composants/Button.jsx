e1211bfb92f142edf5290ae4a1c9ed1fcaf0767e
ksyv <viais.kevin@hotmail.fr>
2025-10-13 13:17:47 +0200

    Add button css

--- tuto_timer/Timer/src/composants/Button.jsx
+++ tuto_timer/Timer/src/composants/Button.jsx
@@ -5,8 +5,8 @@ import ListContainer from "./ListContainer";
 class Button extends Component {
     render() {
         return(
-            <div className='boxed--view'> 
-                <p>Button</p>
+            <div className='button__outline'> 
+                +
             </div>
         )
     }
