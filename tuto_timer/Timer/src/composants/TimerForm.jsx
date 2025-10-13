0c2c4a564480d00a5c976a0ec7e830281e29bd27
ksyv <viais.kevin@hotmail.fr>
2025-10-13 13:27:10 +0200

    Maj TimerForm avec with action display if timer exist or not

--- tuto_timer/Timer/src/composants/TimerForm.jsx
+++ tuto_timer/Timer/src/composants/TimerForm.jsx
@@ -4,9 +4,29 @@ import ListContainer from "./ListContainer";
 
 class TimerForm extends Component {
     render() {
+        const submitText = this.props.title ? "Modifier" : "Créer";
         return(
-            <div className='boxed--view'> 
-                <p>TimerForm</p>
+            <div className='form'> 
+                <div className='form--content'>
+                    <div className='form--item'>
+                        <label>Titre</label>
+                        <input
+                            type='text'
+                            placeholder='Mon Titre'
+                        />
+                    </div>
+                    <div className='form--item'>
+                        <label>Projet</label>
+                        <input
+                            type='text'
+                            placeholder='Mon Projet'
+                        />
+                    </div>
+                </div>
+                <div className='form--button'>
+                    <button className='button btn--submit'>{submitText}</button>
+                    <button className='button btn--cancel'>Annuler</button>
+                </div>
             </div>
         )
     }
